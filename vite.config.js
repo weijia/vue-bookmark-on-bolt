769e4b33eb97b72001682bd07101fd49e9f31fc8
weijiwan <weijiwan@cisco.com>
2025-06-16 09:13:28 +0800

    Added pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,75 @@
 import { defineConfig } from 'vite'
 import vue2 from '@vitejs/plugin-vue2'
+import { VitePWA } from 'vite-plugin-pwa'
 import { fileURLToPath, URL } from 'url'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue2()],
+  plugins: [
+    vue2(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false,
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,gif}'],
+        cleanupOutdatedCaches: true
+      },
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'robots.txt', 'icons/*.png'],
+      manifest: {
+        name: 'Vue Bookmark on Bolt',
+        short_name: 'Bookmarks',
+        description: 'A bookmark management application',
+        theme_color: '#4DBA87',
+        icons: [
+          {
+            src: 'icons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   define: {
     'global': 'window'
   },
